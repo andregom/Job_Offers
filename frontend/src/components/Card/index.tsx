96e940f0c0893ad12cf8c2d78aa450923f30a1ce
andregom <andre.oliveira@ccc.ufcg.edu.br>
2022-05-31 12:44:09 -0300

    creates card component

---
+++ frontend/src/components/Card/index.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import {
+    Box,
+    Card,
+    CardActions,
+    CardContent,
+    Button,
+    Typography
+} from "@material-ui/core";
+
+const bull = (
+    <Box
+        component="span"
+        style={{ display: 'inline-block' }}
+    >
+        •
+    </Box>
+);
+
+export default function BasicCard() {
+    return (
+        <Card style={{ minWidth: 275 }}>
+            <CardContent>
+                <Typography style={{ fontSize: 14 }} gutterBottom>
+                    Word of the Day
+                </Typography>
+                <Typography variant="h5" component="div">
+                    be{bull}nev{bull}o{bull}lent
+                </Typography>
+                <Typography>
+                    adjective
+                </Typography>
+                <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+    );
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>  //that is the key
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
