3f41ff1a6a033a0d8634917814d1a2ce18d87d9d
andregom <andre.oliveira@ccc.ufcg.edu.br>
2022-05-31 08:19:38 -0300

    adds navbar in normal frontend implementation

---
+++ frontend/src/components/Navbar/navbar.tsx
@@ -0,0 +1,50 @@
+import * as singlSpa from 'single-spa';
+
+import {
+  AppBar,
+  Toolbar,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Link,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import "./styles.css";
+
+import DrawerComponent from "./drawer";
+
+import griaLogoSVG from "../assets/gria-logo-topbar.f3a2419c.svg";
+
+export default function Header() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down(900));
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const displayDesktop = () => {
+    return (
+      <Toolbar className="toolbar">
+        <Typography>
+          <img className="appbar-logo" src={griaLogoSVG} alt="logo gria" />
+        </Typography>
+        {isMobile ? (
+          <DrawerComponent />
+        ) : (
+          <div className="nav-link-container">
+            <Link className="nav-link">Vagas</Link>
+            <Link className="nav-link">LOGIN</Link>
+          </div>
+        )}
+      </Toolbar>
+    );
+  };
+
+  return (
+    <header>
+      <AppBar className="gria-appbar">{displayDesktop()}</AppBar>
+    </header>
+  );
+}
